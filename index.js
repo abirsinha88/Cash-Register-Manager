bb717f5220e78642fdd0b89091daf0aca4f06a51
abirsinha88 <abirsinha88@gmail.com>
2025-07-07 21:46:51 +0530

    cash given logic modified and no of notes logic added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let billAmount = document.querySelector('#bill-amount');
+const billAmount = document.querySelector('#bill-amount');
 
-let cashGiven = document.querySelector('#cash-given');
+const cashGiven = document.querySelector('#cash-given');
 let checkButton = document.querySelector('#check-btn');
 let messageParagragh;
+
 
 
 checkButton.addEventListener("click",validateBillAmount);
@@ -19,6 +20,7 @@ function validateBillAmount(){
     
     deleteMessage();
     if(billAmount.value > 0){
+      
         validateCashGiven(); 
     }
     else{
@@ -26,12 +28,24 @@ function validateBillAmount(){
     }
 }
 function validateCashGiven(){
-    if(cashGiven.value >= billAmount.value){
-        const returnAmount = cashGiven.value - billAmount.value;
+
+    if(Number(cashGiven.value) >= Number(billAmount.value)){
+       const returnAmount = cashGiven.value - billAmount.value;
         calculateChange(returnAmount);
     }
     else{
         showMessage("Cash given must be greater than bill amount");
     }
 }
+function calculateChange(returnAmount){
+    let noOfNotes;
+    const availableDenominations = [2000,500,100,20,10,5,1];
+    const numberOfNotes = document.querySelectorAll(".number-of-notes");
+    for(let i = 0 ; i < availableDenominations.length ; i++){
+        noOfNotes = Math.trunc(returnAmount / availableDenominations[i]);
+        returnAmount = returnAmount % availableDenominations[i];
+        numberOfNotes[i].innerText = noOfNotes;
+        
+    }
 
+}
