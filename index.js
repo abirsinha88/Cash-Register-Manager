7773dc2f2532938f1a5760e3bf393a3c1e04c448
abirsinha88 <abirsinha88@gmail.com>
2025-07-07 20:38:37 +0530

    cash given validated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let checkButton = document.querySelector('#check-btn');
 let messageParagragh;
 
 
+checkButton.addEventListener("click",validateBillAmount);
 function showMessage(message){
     messageParagragh= document.createElement("p");
     messageParagragh.innerText = message;
@@ -18,10 +19,19 @@ function validateBillAmount(){
     
     deleteMessage();
     if(billAmount.value > 0){
-
+        validateCashGiven(); 
     }
     else{
         showMessage("Invalid bill amount");
     }
 }
-checkButton.addEventListener("click",validateBillAmount);
+function validateCashGiven(){
+    if(cashGiven.value >= billAmount.value){
+        const returnAmount = cashGiven.value - billAmount.value;
+        calculateChange(returnAmount);
+    }
+    else{
+        showMessage("Cash given must be greater than bill amount");
+    }
+}
+
